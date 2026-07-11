3de9389e0cd64b6ca1bc80f5029ef793ea7ce8a4
zainKhushall <118923762+zainKhushall@users.noreply.github.com>
2026-07-11 11:59:21 +0500

    Update API key and custom search engine ID

--- google_search_handler.py
+++ google_search_handler.py
@@ -3,8 +3,8 @@ import requests
 def google_custom_search(query):
     url = f"https://www.googleapis.com/customsearch/v1"
     params = {
-        "key": 'AIzaSyBD1DhSgSIHDmVkYpD-iwSXPS_MW3xduDE',
-        "cx": '1504deb7f763947f7',
+        "key": 'your-key',
+        "cx": '1504deb7f763947f6',
         "q": query
     }
     response = requests.get(url, params=params)
